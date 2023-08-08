a196e51831b00201a8b12ac9c93a236dbcfc08f0
Mame Gadiaga <mamsogdg@gmail.com>
2023-08-08 19:13:01 +0000

    Retour en arriere

--- src/pages/Cours.jsx
+++ src/pages/Cours.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import CarteSousCours from "../components/CarteSousDomaine";
+import React from 'react'
 
 const Cours = () => {
   return (
-    <div>
-      <CarteSousCours />
+    <div className='text-red-600  bg-white m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl'  >
+      <h1 className='text-red-600 p-11 bg-slate-600'>Cours</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Cours;
+export default Cours
