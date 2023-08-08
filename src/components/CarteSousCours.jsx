af2ece6adb9ed00c11386af2348e957c61631d1a
Mame Gadiaga <mamsogdg@gmail.com>
2023-08-08 20:19:08 +0000

    premiere page integre

---
+++ src/components/CarteSousCours.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+import imgCours from "../medias/téléchargement.jpeg";
+// import ButtonSousCour from "./ButtonSousCours";
+import { BiEditAlt, BiArchive } from "react-icons/bi";
+
+const Card = ({ title, descrip }) => {
+  return (
+    <div className="col-md-4 mb-5">
+      <div className="card border-0 shadow carteCours">
+        <a href="#">
+          <img
+            src={imgCours}
+            className="card-img-top h-auto w-100%"
+            alt="..."
+          />
+        </a>
+        <div className="card-body">
+          <a href="#" className="text-decoration-none text-black titre">
+            <h5 className="card-title">{title}</h5>
+          </a>
+          <p className="card-text">{descrip}</p>
+          <div className="row">
+            <div className="col d-flex align-items-center titre">
+              <BiEditAlt className="text-warning" />
+              Modifier
+            </div>
+            <div className="col d-flex align-items-center titre">
+              <BiArchive className="text-warning" />
+              Archiver
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CarteSousCours = () => {
+  const cardData = [
+    {
+      title: "Titre Cours 1",
+      descrip:
+        "Lorem ipsum1 at FlowParserMixin.parseBlockOrModuleBlockBody at FlowParserMixin.parseBlockOrModuleBlockBody ",
+    },
+    {
+      title: "Titre Cours 2",
+      descrip:
+        "Lorem ipsum2 at FlowParserMixin.parseBlockOrModuleBlockBody at FlowParserMixin.parseBlockOrModuleBlockBody ",
+    },
+    {
+      title: "Titre Cours 3",
+      descrip:
+        "Lorem ipsum3 at FlowParserMixin.parseBlockOrModuleBlockBody at FlowParserMixin.parseBlockOrModuleBlockBody ",
+    },
+    {
+      title: "Titre Cours 4",
+      descrip:
+        "Lorem ipsum4 at FlowParserMixin.parseBlockOrModuleBlockBody at FlowParserMixin.parseBlockOrModuleBlockBody ",
+    },
+    {
+      title: "Titre Cours 5",
+      descrip:
+        "Lorem ipsum5 at FlowParserMixin.parseBlockOrModuleBlockBody at FlowParserMixin.parseBlockOrModuleBlockBody ",
+    },
+    {
+      title: "Titre Cours 6",
+      descrip:
+        "Lorem ipsum6 at FlowParserMixin.parseBlockOrModuleBlockBody at FlowParserMixin.parseBlockOrModuleBlockBody ",
+    },
+    // Ajoutez d'autres objets de données ici
+  ];
+
+  const chunkedData = [];
+  const chunkSize = 3;
+
+  for (let i = 0; i < cardData.length; i += chunkSize) {
+    chunkedData.push(cardData.slice(i, i + chunkSize));
+  }
+
+  return (
+    <div className="container p-5">
+      {chunkedData.map((chunk, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {chunk.map((data, index) => (
+            <Card
+              key={index}
+              title={data.title}
+              descrip={data.descrip}
+              image={data.image}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CarteSousCours;
