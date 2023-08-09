71d02e3adad22c7ae88730d05011f778cdc9424a
Makhan Diakho <makhandiakho95@gmail.com>
2023-08-09 14:34:48 +0000

    dahbord admin

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+// import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from "chart.js";
+// ChartJS.register(
+//     BarElement, CategoryScale, LinearScale, Tooltip, Legend
+// )
+const BarChart = ({chartData}) => {
+  return (
+     <Bar data={chartData} />
+  )
+}
+
+export default BarChart
