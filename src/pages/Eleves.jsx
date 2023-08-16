46fb05a5f24068e999c13b020688d0f4695269bf
unknown <mamsogdg@gmail.com>
2023-08-16 13:22:47 +0000

    Retour

--- src/pages/Eleves.jsx
+++ src/pages/Eleves.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import TachesValidations from './TachesValidations'
 
 const Eleves = () => {
   return (
-    <div className='text-red-600  bg-white m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl'>
-      <h1 className='text-red-600 p-11 bg-slate-600'>LISTES ELEVES</h1>
-    </div>
+   <div>
+    <TachesValidations />
+   </div>
   )
 }
 
---
+++ src/pages/TachesValidations.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TachesValidations = () => {
+    return (
+        <div>
+            <h1>TACHES VALIDATIONS</h1>
+        </div>
+    );
+};
+
+export default TachesValidations;
