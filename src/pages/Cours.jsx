00d5fe1df09657e50b4a78b73eaf84622f0c76d7
Mama GDG <mamsogdg@gmail.com>
2023-08-18 19:06:52 +0000

    Modifier image

--- src/pages/Cours.jsx
+++ src/pages/Cours.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Domaine from "../components/cardDomaines/Domaine";
 import ButtonReutilisable from "../components/ButtonReutilisable";
-import Parametre from "./parametres/Parametre";
 
 const Cours = () => {
   const domains = [
@@ -16,73 +15,72 @@ const Cours = () => {
     },
   ];
   return (
-    // <div>
-    //   <div className="bg-white m-5 p-5 rounded-3xl domaine">
-    //     <div className="container ">
-    //       <div className="row d-flex align-items-center mt-5">
-    //         <div className="col-md-6 col-sm-12">
-    //           <h1 className="">Les Domaines</h1>
-    //         </div>
-    //         <div className="col-md-6 col-sm-12">
-    //           <div data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-    //             <ButtonReutilisable text={"Ajouter un domaine"} />
-    //           </div>
-    //           {/* button modal */}
-    //           {/* Modal */}
-    //           <div
-    //             className="modal fade"
-    //             id="staticBackdrop"
-    //             data-bs-backdrop="static"
-    //             data-bs-keyboard="false"
-    //             tabindex="-1"
-    //             aria-labelledby="staticBackdropLabel"
-    //             aria-hidden="true"
-    //           >
-    //             <div className="modal-dialog modal-dialog-centered">
-    //               <div className="modal-content">
-    //                 <div className="modal-header">
-    //                   <h1 className="modal-title fs-5" id="staticBackdropLabel">
-    //                     Ajout de domaine
-    //                   </h1>
-    //                   <button
-    //                     type="button"
-    //                     className="btn-close"
-    //                     data-bs-dismiss="modal"
-    //                     aria-label="Close"
-    //                   ></button>
-    //                 </div>
-    //                 <div className="modal-body">
-    //                   <div class="form-floating mb-3">
-    //                     <input
-    //                       type="text"
-    //                       class="form-control"
-    //                       id="floatingInput"
-    //                       placeholder="Nom du domaine"
-    //                     />
-    //                     <label for="floatingInput">Nom du domaine</label>
-    //                   </div>
-    //                 </div>
-    //                 <div className="modal-footer">
-    //                   <ButtonReutilisable text={"Ajouter"} />
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="container my-5">
-    //       <div className="row row-gap-3">
-    //         {domains.map((domain) => (
-    //           <div className="col-lg-4 col-md-6 col-sm-12">
-    //             <Domaine title={domain.title} />
-    //           </div>
-    //         ))}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <Parametre />
+    <div>
+      <div className="bg-white m-5 p-5 rounded-3xl domaine">
+        <div className="container ">
+          <div className="row d-flex align-items-center mt-5">
+            <div className="col-md-6 col-sm-12">
+              <h1 className="">Les Domaines</h1>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <div data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                <ButtonReutilisable text={"Ajouter un domaine"} />
+              </div>
+              {/* button modal */}
+              {/* Modal */}
+              <div
+                className="modal fade"
+                id="staticBackdrop"
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                tabindex="-1"
+                aria-labelledby="staticBackdropLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog modal-dialog-centered">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                        Ajout de domaine
+                      </h1>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="floatingInput"
+                          placeholder="Nom du domaine"
+                        />
+                        <label for="floatingInput">Nom du domaine</label>
+                      </div>
+                    </div>
+                    <div className="modal-footer">
+                      <ButtonReutilisable text={"Ajouter"} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container my-5">
+          <div className="row row-gap-3">
+            {domains.map((domain) => (
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <Domaine title={domain.title} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
