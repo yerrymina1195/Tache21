b285ec7dbc861f88635979385d9d5cac4a730caa
Mama GDG <mamsogdg@gmail.com>
2023-08-22 14:33:43 +0000

    Reintegration

--- src/pages/Livraisons.jsx
+++ src/pages/Livraisons.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '.';
+
 
 const Livraisons = () => {
   return (
 
 
-    <div className=' bg-backthird m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl'>
-       <h1 className=' text-couleur2 p-11 bg-slate-600'>LIvraisons</h1>
+    // <div className=' bg-backthird m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl'>
+    //    <h1 className=' text-couleur2 p-11 bg-slate-600'>LIvraisons</h1>
 
-    </div>
+    // </div>
+
+    <div>
+    <div class="mabg">sabddjbsjdbsjbjsdbsjsdbjbsjsbbjslbs</div>
+<div class="mabg mabg2"></div>
+<div class="mabg mabg3"></div>
+<div class="macontent">
+<h1 class="mah1">Sliding Diagonals Background Effect</h1>
+</div>
+  </div>
+
+   
   )
 }
 
