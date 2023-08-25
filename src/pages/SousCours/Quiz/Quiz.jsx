e1f56ad2b01aae8d52297bae4dfa9464e13e2dd7
mamadouniang19 <niang132526@gmail.com>
2023-08-25 10:23:30 +0100

    modifier quiz

--- src/pages/SousCours/Quiz/Quiz.jsx
+++ src/pages/SousCours/Quiz/Quiz.jsx
@@ -1,217 +1,178 @@
-import React from "react";
-import Input from "./Input";
-import Text from "./Text";
+import React, { useState } from "react";
 
-export default function Quiz({ props }) {
+export default function Quiz() {
+  const [answers, setAnswers] = useState(Array(10).fill(""));
+  const [score, setScore] = useState(null);
+
+  const questions = [
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 3// L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    {
+      question: "1- Quelle est la différence entre Bootstrap et CSS?",
+      options: [
+        "a- Bootstrap est un framework.",
+        "b- CSS est un framework.",
+        "c- Ils sont identiques.",
+        "d- Aucune des réponses ci-dessus."
+      ],
+      correctAnswer: 0 // L'indice de la réponse correcte dans le tableau options
+    },
+    // Définissez vos autres questions ici de la même manière
+  ];
+
+  const handleAnswerSelect = (questionIndex, selectedOptionIndex) => {
+    const updatedAnswers = [...answers];
+    updatedAnswers[questionIndex] = selectedOptionIndex;
+    setAnswers(updatedAnswers);
+  };
+
+  const calculateScore = () => {
+    let totalScore = 0;
+    answers.forEach((selectedOptionIndex, questionIndex) => {
+      const question = questions[questionIndex];
+      if (selectedOptionIndex === question.correctAnswer) {
+        totalScore++;
+      }
+    });
+    return totalScore;
+  };
+
+  const handleSubmit = () => {
+    const totalScore = calculateScore();
+    setScore(totalScore);
+  };
+
+  const renderQuestions = () => {
+    return questions.map((question, questionIndex) => (
+      <div className="flex-col" key={questionIndex}>
+        <p>{question.question}</p>
+        {question.options.map((option, optionIndex) => (
+         <label className="flex-col" key={optionIndex}>
+            <input
+              type="radio"
+              name={`question_${questionIndex}`}
+              value={optionIndex}
+              onChange={() => handleAnswerSelect(questionIndex, optionIndex)}
+              checked={answers[questionIndex] === optionIndex}
+            />
+            {option} 
+          </label> 
+        ))}
+      </div>
+    ));
+  };
+
   return (
     <div className="bg-white px-5">
-      <h1 className="text-center ">Quiz</h1>
+      <h1 className="text-center">Quiz</h1>
       <p className="mb-5">
-        Découvre tes connaissances avec notre quiz interactif ! Réponds aux
-        questions pour voir à quel point tu maîtrises le sujet.!Ce quiz est
-        conçu pour tester tes connaissances sur le sujet passionnant que nous
-        avons étudié. Il comporte une série de questions à choix multiple, où tu
-        devras sélectionner la réponse qui te semble correcte. Chaque question
-        est accompagnée d'une seule réponse correcte parmi les options fournies.
+        Découvre tes connaissances avec notre quiz interactif !
       </p>
-      <div>
-        <Text texte={"1- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"2- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"3- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"4- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"5- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"6- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"7- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"8- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"9- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
-      <div>
-        <Text texte={"10- Quel est la difference entre bootstrap et css?"} />
-        <hr />
-        <p className="d-flex">
-          a-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          b-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          c-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          d-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-        <p className="d-flex">
-          e-<Input /><span className="ms-3">bootstrap est un framework.</span> 
-        </p>
-      </div>
 
-      <button
-        type="submit"
-        className="prev text-white align-items-center justify-content-center d-flex"
-      >
-        {" "}
-        Envoyer
-      </button>
+      {renderQuestions()}
+      <br />
+      {score === null ? (
+
+        <button
+          type="button"
+          className="prev text-white align-items-center justify-content-center d-flex"
+          onClick={handleSubmit}
+        >
+          Terminer
+        </button> 
+      ) : (
+      
+        
+        <p className="text-center mt-5 w-25 h-10 justify-start bg-couleur2 font-bold">Score : {score} / {questions.length}</p>
+      )}
     </div>
   );
 }
