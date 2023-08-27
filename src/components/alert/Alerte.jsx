4eca32f28e018c61b139a4a63949179e9fc983a1
Mama GDG <mamsogdg@gmail.com>
2023-08-27 12:21:12 +0000

    alerte

--- src/components/alert/Alerte.jsx
+++ src/components/alert/Alerte.jsx
@@ -3,13 +3,14 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../alert/Alerte.css'; 
 
-const Alerte = () => {
+const Alerte = () => {  
   const notify = () => {
     toast.success('Supprimé avec succès', {
       position: toast.POSITION.TOP_RIGHT,
     });
-  };
-
+  };                                                                          
+      
+   
   return (
     <div>
       <button onClick={notify}>Afficher la notification</button>
@@ -17,7 +18,7 @@ const Alerte = () => {
         progressClassName="my-toast-progress-bar" 
       />
     </div>
-  );
+  );      
 };
 
 export default Alerte;
