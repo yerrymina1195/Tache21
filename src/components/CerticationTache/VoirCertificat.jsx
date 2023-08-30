3c2707cf9b89327a6e7f0b7c396f2ecc4ad81f04
rama.fall <ramafall49@gmail.com>
2023-08-30 16:42:10 +0000

    logique voir certification deja gerer

--- src/components/CerticationTache/VoirCertificat.jsx
+++ src/components/CerticationTache/VoirCertificat.jsx
@@ -7,6 +7,10 @@ import img3 from '../../data/logo.png';
 
 function VoirCertificat() {
     const [name, setName] = useState('');
+    const [date, setDate] = useState('');
+    const [coach, setCoach] = useState('');
+    const [modul, setModul] = useState('');
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -41,42 +45,75 @@ function VoirCertificat() {
                         </div>
                         <button>Generate</button>
                     </div> */}
-                    <div className="container bgCertificate p-4">
-  <div className="bgCertification2">
-    <div className="p-3">
-      <div className="row text-center d-flex justify-content-center align-items-center flex-column">
-        <h1 className="digi p-3 text-white mb-5 titre">
-          DIGISPHERE <br /> <span className="fs-5">E-LEARNING</span>
-        </h1>
-        <p>Ceci certifie que</p>
-        <h4>Mame GADIAGA</h4>
-        <p>
-          a terminé avec succés
-          <br /> 
-          le module programmation le 30 Juillet 2023
-        </p>
-      </div>
-      <div className="row mt-5 text-center">
-        <div className="col-4 text-center d-flex justify-content-center align-items-center">
-          <img src={img1} alt="scan" className="wscan" />
-        </div>
-        <div className="col-4 psign p-3 text-center d-flex justify-content-center align-items-center flex-column">
-          <h4 className="border-bottom titre">SIGNATURE</h4>
-          <img src={img2} alt="sign" className="sign" />
-        </div>
-        <div className="col-4 text-center d-flex justify-content-center align-items-center">
-          <img src={img3} alt="certif" className="logo" />
-        </div>
-      </div>
-      <div className="row text-center mt-5">
-        <p>
-          Validé par: <br />{" "}
-          <span className="text-couleur1">Jésus Christ Diatta</span>
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
+
+
+
+                    <div className="card-header  dark:bg-secondary-dark-bg text-white dark:text-gray-200">Metter vos informations au complet</div>
+                    <div class="form-floating mb-3 shadow p-5">
+
+                        <div className="row mb-3">
+                            <div className="col-6">
+                                <label for="floatingInput">votre nom complet:</label>
+                                <input className="form-control txt" id="floatingInput" type="text" name="txt" value={name} onChange={(e) => setName(e.target.value)} required />
+                            </div>
+                            <div className="col-6">
+                                <label for="floatingInput">la date:</label>
+                                <input className="form-control date" id="floatingInput" type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6">
+                                <label for="floatingInput">module</label>
+                                <input className="form-control modul" id="floatingInput" type="text" name="modul" value={modul} onChange={(e) => setModul(e.target.value)} required />
+                            </div>
+                            <div className="col-6">
+                                <label for="floatingInput">valide par</label>
+                                <input className="form-control coach" id="floatingInput" name="coach" value={coach} onChange={(e) => setCoach(e.target.value)} required />
+                            </div>
+                        </div>
+
+<div className='align-items-center justify-content-center d-flex'>
+                        <button className="main-btn w-25 p-2 fw-bold mt-3 cert text-white ">Envoyer</button></div>
+                    </div>
+
+
+                    <div className="container bgCertificate p-4 mt-5" style={{ display: 'none' }}>
+                        <div id="pdf-content" className="bgCertification2" >
+                            <div className="p-3">
+                                <div className="row text-center d-flex justify-content-center align-items-center flex-column ">
+                                    <h1 className="digi p-3 text-white mb-5 titre">
+                                        DIGISPHERE <br /> <span className="fs-5">E-LEARNING</span>
+                                    </h1>
+                                    <p>Ceci certifie que</p>
+                                    <h4>{name}</h4>
+                                    <p>
+                                        a terminé avec succés
+                                        <br />
+                                        le module <span className='fw-bold'>{modul}</span><span> le {date}</span>
+                                    </p>
+                                </div>
+                                <div className="row mt-5 text-center">
+                                    <div className="col-4 text-center d-flex justify-content-center align-items-center">
+                                        <img src={img1} alt="scan" className="wscan" />
+                                    </div>
+                                    <div className="col-4 psign p-3 text-center d-flex justify-content-center align-items-center flex-column">
+                                        <h4 className="border-bottom titre">SIGNATURE</h4>
+                                        <img src={img2} alt="sign" className="sign" />
+                                    </div>
+                                    <div className="col-4 text-center d-flex justify-content-center align-items-center">
+                                        <img src={img3} alt="certif" className="logo" />
+                                    </div>
+                                </div>
+                                <div className="row text-center mt-5">
+                                    <p>
+                                        Validé par son coach: <br />{" "}
+                                        <span className="text-couleur1">{coach}</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
 
 
                 </form>
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -10,8 +10,8 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 
 const Notification = () => {
-  
-  const { currentColor ,notif } = useStateContext();
+
+  const { currentColor, notif, certifie, item } = useStateContext();
 
 
 
@@ -23,22 +23,32 @@ const Notification = () => {
         <div className="flex gap-3">
           <button type="button" className="text-white text-xs rounded p-1 px-2 bg-orange-theme "> 5 New</button>
         </div>
-        <Button icon={<MdOutlineCancel />} color="rgb(153, 171, 180)" bgHoverColor="light-gray" size="2xl" borderRadius="50%" /> 
+        <Button icon={<MdOutlineCancel />} color="rgb(153, 171, 180)" bgHoverColor="light-gray" size="2xl" borderRadius="50%" />
       </div>
       <div className="">
-        {notif?.map((item, index) => (
-          <div key={index} className="flex items-center leading-8 gap-5 border-b-1 border-color ">
-            <img className="rounded-full h-10 w-10" src={item.notifieA} alt={item.message} />
-            <div>
-              <p className="font-semibold dark:text-gray-200">{item.notifiepar}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.idcours} </p>
-            </div>
-          </div>
-        ))}
+      {notif ?.map((item, index) => (
+  <div key={index} className="flex items-center leading-8 gap-5 border-b-1 border-color ">
+    <img className="rounded-full h-10 w-10" src={item.notifieA} alt={item.message} />
+    <div>
+      <p className="font-semibold dark:text-gray-200">{item.notifiepar}</p>
+
+
+      {item.idcours ? (
+        <p className="text-gray-500 text-sm dark:text-gray-400">{item.idcours}</p>
+      ) : (
+        <p className="text-gray-500 text-sm dark:text-gray-400">{item.lien}</p>
+      )}
+
+
+
+
+    </div>
+  </div>
+))}
         <div className="mt-5">
           <Button color="white" bgColor={currentColor} text="See all notifications" borderRadius="10px" width="full" />
         </div>
-      </div> 
+      </div>
     </div>
   );
 };
